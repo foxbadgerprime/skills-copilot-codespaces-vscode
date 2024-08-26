eb369cfd93a5097de9261e362097117b43c86a76
Rodney Johnson <140769469+foxbadgerprime@users.noreply.github.com>
2024-08-26 20:27:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+    return [
+        {
+            id: '1',
+            name: 'JavaScript',
+            value: 80
+        },
+        {
+            id: '2',
+            name: 'TypeScript',
+            value: 70
+        },
+        {
+            id: '3',
+            name: 'Angular',
+            value: 85
+        }
+    ];
+}
